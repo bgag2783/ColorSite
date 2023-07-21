f8bfbae6793e713412d08575f4b51c8f777b2244
Brian Gill <bgag1111@gmail.com>
2023-07-22 00:25:39 +1200

    Make it a PWA

---
+++ sw.js
@@ -0,0 +1,26 @@
+const cacheName = 'color-comparator-v1';
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/scripts.js',
+    '/manifest.json',
+    '/icon-192x192.png',
+    '/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(assetsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
